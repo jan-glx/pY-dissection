bc8129cd4d94d3dd91d4ebf7fea3a303dd36dfab
Jan Gleixner <jan.gleixner@gmail.com>
2015-08-04 14:39:15 +0200

    reformatting code

--- main.R
+++ main.R
@@ -7,42 +7,56 @@ library(stringi)
 library(stringr)
 
 # read all grossman et al data from intact --------------------------------------------------------------------
-rm(list=ls())
-df <- read.table('../Data/IM-22632.txt',sep='\t',quote="",stringsAsFactors=F,header=T,comment.char = "")#,stringsAsFactors=F
+rm(list = ls())
+df <-
+  read.table(
+    '../Data/IM-22632.txt',sep = '\t',quote = "",stringsAsFactors = F,header =
+      T,comment.char = ""
+  )#,stringsAsFactors=F
 #discard irrelevant columns
-df %<>% select(Feature.s..interactor.A,
-               Feature.s..interactor.B,
-               Identification.method.participant.A,
-               Identification.method.participant.B,
-               Annotation.s..interactor.A,
-               Annotation.s..interactor.B,
-               Xref.s..interactor.B,
-               Xref.s..interactor.A,
-               Experimental.role.s..interactor.A,
-               Experimental.role.s..interactor.B,
-               ID.s..interactor.A = X.ID.s..interactor.A,
-               ID.s..interactor.B,
-               Host.organism.s.,
-               Interaction.annotation.s.,
-               Interaction.identifier.s.,
-               Confidence.value.s.,
-               Taxid.interactor = Taxid.interactor.B,
-               Interaction.type.s.,
-               Interaction.detection.method.s.)
+df %<>% select(
+  Feature.s..interactor.A,
+  Feature.s..interactor.B,
+  Identification.method.participant.A,
+  Identification.method.participant.B,
+  Annotation.s..interactor.A,
+  Annotation.s..interactor.B,
+  Xref.s..interactor.B,
+  Xref.s..interactor.A,
+  Experimental.role.s..interactor.A,
+  Experimental.role.s..interactor.B,
+  ID.s..interactor.A = X.ID.s..interactor.A,
+  ID.s..interactor.B,
+  Host.organism.s.,
+  Interaction.annotation.s.,
+  Interaction.identifier.s.,
+  Confidence.value.s.,
+  Taxid.interactor = Taxid.interactor.B,
+  Interaction.type.s.,
+  Interaction.detection.method.s.
+)
 
 df %<>% gather(key,value, ends_with(".A"), ends_with(".B")) %>%
   extract(key,c("var","Interactor"),"(.*)\\.([AB])$") %>%
   spread(var,value)
 
 
-df %<>% extract(Confidence.value.s.,c("confidence"),"^intact\\-miscore\\:(.*)$",convert=T) %>%
-  extract(Experimental.role.s..interactor,c("role"),'^psi\\-mi\\:"MI\\:(?:\\d+)"\\((prey|bait|neutral)(?: component)?\\)$',convert=T)  %>%
-  separate(ID.s..interactor,c("Interactor_ID_db","Interactor_ID"),'\\:',convert=F) 
+df %<>% extract(Confidence.value.s.,c("confidence"),"^intact\\-miscore\\:(.*)$",convert =
+                  T) %>%
+  extract(
+    Experimental.role.s..interactor,c("role"),'^psi\\-mi\\:"MI\\:(?:\\d+)"\\((prey|bait|neutral)(?: component)?\\)$',convert =
+      T
+  )  %>%
+  separate(ID.s..interactor,c("Interactor_ID_db","Interactor_ID"),'\\:',convert =
+             F)
 
 # Map some ORFs that indentfied by intact IDs to their corresponding Uniprot IDs
-to_map = df %>% filter(Interactor_ID_db == "intact") %>% 
-  select(Interactor_ID_db, Interactor_ID,Xref.s..interactor) %>% 
-  extract(Xref.s..interactor,c("From"),"refseq:([A-Z]+\\_\\d+(?:.\\d+)?)(?:[(|].*)?$",remove=T,convert=F) %>%
+to_map = df %>% filter(Interactor_ID_db == "intact") %>%
+  select(Interactor_ID_db, Interactor_ID,Xref.s..interactor) %>%
+  extract(
+    Xref.s..interactor,c("From"),"refseq:([A-Z]+\\_\\d+(?:.\\d+)?)(?:[(|].*)?$",remove =
+      T,convert = F
+  ) %>%
   distinct()
 
 url = 'http://www.uniprot.org/mapping/'
@@ -50,57 +64,76 @@ params =   c(
   'from', 'REFSEQ_NT_ID',
   'to', 'ACC',
   'format', 'tab',
-  'query', paste(to_map$From, collapse=" ")
+  'query', paste(to_map$From, collapse = " ")
 )
 params = lapply(params, URLencode)
 query = paste(params[c(T,F)], params[c(F,T)], sep = "=", collapse = "&")
-mapping = read.table(paste0(url,'?',query), header = T, stringsAsFactors=F)
+mapping = read.table(paste0(url,'?',query), header = T, stringsAsFactors =
+                       F)
 mapping %<>% right_join(to_map)
 mapping
 
-df %<>% left_join(select(mapping, -From))
-df %<>% 
+df %<>% left_join(select(mapping,-From))
+df %<>%
   mutate(Interactor_ID = ifelse(Interactor_ID_db == "intact",To, Interactor_ID)) %>%
   select(-Interactor_ID_db,-To)
 # done mapping, save relevant data
-write.table(df, "../Data/all_intact_data_from_grossmanEtAl.tsv", sep="\t", row.names = F, qmethod="double")
+write.table(
+  df, "../Data/all_intact_data_from_grossmanEtAl.tsv", sep = "\t", row.names = F, qmethod =
+    "double"
+)
 
 
 
 # getting sequences for ORFs without Uniprotkb ID
-url='http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=fasta_cds_aa&id='
-sequences = data.frame(seq=sapply((mapping %>% filter(is.na(To)))$From, 
-                                  function(From){paste0(readLines(paste0(url,From))[-1], collapse="")}))
+url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=fasta_cds_aa&id='
+sequences = data.frame(seq = sapply((mapping %>% filter(is.na(
+  To
+)))$From,
+function(From) {
+  paste0(readLines(paste0(url,From))[-1], collapse = "")
+}))
 sequences$From = rownames(sequences)
 rownames(sequences) <- NULL
 # and do nothing with it
 
 
 # Loading sequences and know modifications from uniprot for all ORFst --------------------------------------------------------------------
-rm(list=ls())
-df=read.table("../Data/all_intact_data_from_grossmanEtAl.tsv", sep="\t",header = T,stringsAsFactors = F)
+rm(list = ls())
+df = read.table(
+  "../Data/all_intact_data_from_grossmanEtAl.tsv", sep = "\t",header = T,stringsAsFactors = F
+)
 
-uniprot_ACCs = data.frame(ID=unique(toupper(df$Interactor_ID[!is.na(df$Interactor_ID)])),stringsAsFactors = F) %>% 
-  extract(ID,c("canonic","isoform"),"^([^\\-]*)(?:\\-(.*))?$", remove= F)
+uniprot_ACCs = data.frame(ID = unique(toupper(df$Interactor_ID[!is.na(df$Interactor_ID)])),stringsAsFactors = F) %>%
+  extract(ID,c("canonic","isoform"),"^([^\\-]*)(?:\\-(.*))?$", remove = F)
 uniprot_ACCs_canonic = unique(uniprot_ACCs$canonic)
 
 url = 'http://www.uniprot.org/uniprot/'
-uniseq=NULL
-for (uniprot_ACCs_canonic_i in split(uniprot_ACCs_canonic,floor(0:(length(uniprot_ACCs_canonic)-1)/100))){
+uniseq = NULL
+for (uniprot_ACCs_canonic_i in split(uniprot_ACCs_canonic,floor(0:(length(
+  uniprot_ACCs_canonic
+) - 1) / 100))) {
   params =   c(
-    'query', paste0('"accesion+',uniprot_ACCs_canonic_i,'"', collapse="OR"),
+    'query', paste0('"accesion+',uniprot_ACCs_canonic_i,'"', collapse = "OR"),
     'columns', 'id,sequence,feature(MODIFIED RESIDUE),feature(ALTERNATIVE SEQUENCE),comment(ALTERNATIVE PRODUCTS)',
     'format', 'tab'
   )
   params = lapply(params, URLencode)
   query = paste(params[c(T,F)], params[c(F,T)], sep = "=", collapse = "&")
-  uniseq = rbind(uniseq,read.table(paste0(url,'?',query), quote="",comment.char = "", header = T, stringsAsFactors=F, sep="\t"))
+  uniseq = rbind(
+    uniseq,read.table(
+      paste0(url,'?',query), quote = "",comment.char = "", header = T, stringsAsFactors =
+        F, sep = "\t"
+    )
+  )
   print(paste("fetched from uniprot:", nrow(uniseq)))
 }
 
-uniprot_data <- left_join(uniprot_ACCs, uniseq, by = c("canonic" = "Entry"))
-dropped = (uniprot_data  %>% filter(stri_length(uniprot_data$Sequence)==0))$ID 
-if(length(dropped)!=0) {
+uniprot_data <-
+  left_join(uniprot_ACCs, uniseq, by = c("canonic" = "Entry"))
+dropped = (uniprot_data  %>% filter(stri_length(uniprot_data$Sequence) ==
+                                      0))$ID
+if (length(dropped) != 0) {
   warning(
     paste0(
       "dropped Entries with Interactor(s) ",
@@ -108,7 +141,7 @@ if(length(dropped)!=0) {
       " as the UniProt Isoform identifier could not be mapped to a sequence"
     )
   )
-  uniprot_data %<>% filter(stri_length(uniprot_data$Sequence)!=0)
+  uniprot_data %<>% filter(stri_length(uniprot_data$Sequence) != 0)
 }
 
 
@@ -116,69 +149,88 @@ quotemeta <- function(string) {
   gsub("([.|()\\^{}+$*?]|\\[|\\])", "\\\\\\1", string)
 }
 
-parse_alternative_sequence <- function(alt_seq_field, alt_prod_field, isoform_id, canonical_sequence) {
-  canonical_sequence <- strsplit(canonical_sequence,"")[[1]]
-  pattern <- paste0("Name=([^ ;]+)(?: \\{[^\\}]*\\})?;(?: Synonyms=[^;]+;)? IsoId=",quotemeta(isoform_id))
-  isoform = str_match(alt_prod_field,pattern)[2]
-  if(is.na(isoform)){
-    warning(paste("isoform",isoform_id,"not found"))
-    return(NA_character_)
+parse_alternative_sequence <-
+  function(alt_seq_field, alt_prod_field, isoform_id, canonical_sequence) {
+    canonical_sequence <- strsplit(canonical_sequence,"")[[1]]
+    pattern <-
+      paste0(
+        "Name=([^ ;]+)(?: \\{[^\\}]*\\})?;(?: Synonyms=[^;]+;)? IsoId=",quotemeta(isoform_id)
+      )
+    isoform = str_match(alt_prod_field,pattern)[2]
+    if (is.na(isoform)) {
+      warning(paste("isoform",isoform_id,"not found"))
+      return(NA_character_)
+    }
+    
+    pattern <-
+      paste0(
+        "VAR_SEQ (\\d+) (\\d+) (?:(Missing)|(\\w+) \\-\\> (\\w+)) \\(in [^\\)]*isoform ",
+        quotemeta(isoform),"[^\\)]*\\)."
+      )
+    df = str_match_all(alt_seq_field,pattern)
+    #cat(length(df))
+    df = data.frame(df[[1]],stringsAsFactors = F)
+    colnames(df) <-
+      c("whole_match","start_base","end_base","Missing","From","To")
+    if (nrow(df) == 0) {
+      warning(paste("isoform",isoform,"(",isoform_id,")","not found"))
+      return(NA_character_)
+    }
+    df %<>%  mutate_each(funs(as.numeric),start_base,end_base)
+    
+    df %<>% rowwise() %>% dplyr::mutate(mechismo_dif_string = ifelse(
+      Missing == "Missing",
+      paste0(
+        canonical_sequence[start_base:end_base],
+        as.character(start_base:end_base),
+        "X",
+        collapse = " "
+      ),
+      paste0(
+        strsplit(From,"")[[1]],
+        as.character(start_base:end_base),
+        c(To,rep("X",end_base - start_base)),
+        collapse = " "
+      )
+    ))
+    paste(df$mechismo_dif_string, collapse = " ")
   }
-  
-  pattern <- paste0("VAR_SEQ (\\d+) (\\d+) (?:(Missing)|(\\w+) \\-\\> (\\w+)) \\(in [^\\)]*isoform ",
-                    quotemeta(isoform),"[^\\)]*\\).")
-  df=str_match_all(alt_seq_field,pattern)
-  #cat(length(df))
-  df=data.frame(df[[1]],stringsAsFactors=F)
-  colnames(df) <- c("whole_match","start_base","end_base","Missing","From","To")
-  if(nrow(df)==0){
-    warning(paste("isoform",isoform,"(",isoform_id,")","not found"))
-    return(NA_character_)
-  }
-  df %<>%  mutate_each(funs(as.numeric),start_base,end_base)
-  
-  df %<>% rowwise() %>% dplyr::mutate(
-    mechismo_dif_string = ifelse(Missing == "Missing", 
-                                 paste0(canonical_sequence[start_base:end_base],
-                                        as.character(start_base:end_base), 
-                                        "X", 
-                                        collapse=" "),
-                                 paste0(strsplit(From,"")[[1]],
-                                        as.character(start_base:end_base),
-                                        c(To,rep("X",end_base-start_base)), 
-                                        collapse=" "))
-  )
-  paste(df$mechismo_dif_string, collapse = " ")
-}
 
-uniprot_data=  uniprot_data %>% rowwise() %>%  
-  dplyr::mutate(mechismo_dif_string = 
-                  ifelse(isoform == "", 
-                         "",
-                         parse_alternative_sequence(Alternative.sequence,Alternative.products..isoforms.,
-                                                    ID,Sequence))) %>% ungroup()
+uniprot_data =  uniprot_data %>% rowwise() %>%
+  dplyr::mutate(mechismo_dif_string =
+                  ifelse(
+                    isoform == "",
+                    "",
+                    parse_alternative_sequence(
+                      Alternative.sequence,Alternative.products..isoforms.,
+                      ID,Sequence
+                    )
+                  )) %>% ungroup()
 
 head(uniprot_data)
 
-canonical2isoform <- function(canonical_sequence, mechismo_dif_string){
-  sequence = canonical_sequence
-  ops = strsplit(mechismo_dif_string, " ")
-  idx_map=1:nchar(canonical_sequence)
-  for (op in ops){
-    if(nchar(op)==3){
-      print(2)
-    }else{
-      print(4)
+canonical2isoform <-
+  function(canonical_sequence, mechismo_dif_string) {
+    sequence = canonical_sequence
+    ops = strsplit(mechismo_dif_string, " ")
+    idx_map = 1:nchar(canonical_sequence)
+    for (op in ops) {
+      if (nchar(op) == 3) {
+        print(2)
+      }else{
+        print(4)
+      }
     }
   }
-}
 
-write.table(uniprot_data, "../Data/uniprotdata_4all_hits.tsv", sep="\t", row.names = F)
+write.table(
+  uniprot_data, "../Data/uniprotdata_4all_hits.tsv", sep = "\t", row.names = F
+)
 
 # extract(df$Interaction.annotation.s.,c(),
 #   "[comment:\"\\\"Phosphorylation-dependent interaction. The interaction is only detected in the presence of the following kinases: (.+\\(.+\\),)*\\.\\\"
 # \"|comment:\"\\\"The interaction failed to show if kinase-dead versions of ABL2 (P42684) or FYN (P06241) were used.\\\"\"|figure legend:Suppl. table S3, suppl. fig. S4|full coverage:Only protein-protein interactions|curation depth:imex curation)
-# 
-# 
+#
+#
 
 #unique(unlist(lapply(df$Interaction.annotation.s.,function(x){strsplit(x,split="\\|")})))
