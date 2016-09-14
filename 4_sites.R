673196f97919ea39df72b2fdbe640e2d7d7cd3f6
Jan Gleixner <jan.gleixner@gmail.com>
2016-09-14 18:29:30 +0200

    cleanup, rename files to indicate execution order

---
+++ 4_sites.R
@@ -0,0 +1,161 @@
+source("misc.R")
+
+
+
+# finding sites and uniprot info for them ----------
+uniprot_data = fread("../Data/uniprotdata_4all_hits.tsv",sep="\t",
+                     select=c("canonic","Entry.name", "ID","Gene.names...primary..", "Sequence","Phosphotyrosins"))
+setnames(uniprot_data,"Gene.names...primary..","gene_name")
+
+uniprot_data[,Phosphotyrosins:=lapply(str_split(Phosphotyrosins,","),as.integer)]
+uniprot_data[is.na(Phosphotyrosins),Phosphotyrosins:=lapply(Phosphotyrosins,function(x){integer()})]
+
+sites=uniprot_data[,.(res=str_locate_all(Sequence,'Y')[[1]][,1],
+                      seq_length=str_length(Sequence)),
+                   by=.(canonic,ID,Entry.name, gene_name,Sequence)][!is.na(res)]
+
+sites[,neighbourhood:=paste0(str_sub("_______",0,pmax(0,8-res)),
+                             str_sub(Sequence,pmax(1,res-7),pmin(seq_length,res+7)),
+                             str_sub("_______",0,pmax(0,7-(seq_length-res))))][,Sequence:=NULL]
+
+
+setkey(uniprot_data,ID)
+setkey(sites,ID)
+setkey(uniprot_data,canonic)
+setkey(sites,canonic)
+
+sites[uniprot_data,phosphorylated_by_uniprot:= mapply('%in%',res, Phosphotyrosins) ]
+
+# adding evidence from phophositeplus -------------
+
+psp = fread("../Data/Phosphorylation_site_dataset.tsv",sep = "\t",sep2="; ",#colClasses = c(`CST_CAT#`="character"),
+            select=c("ACC_ID","GENE","MOD_RSD","SITE_+/-7_AA","LT_LIT","MS_LIT","MS_CST"))
+setnames(psp,"SITE_+/-7_AA","neighbourhood")
+
+psp[,c("res_kind","res"):=data.table(str_match(MOD_RSD,"([YST])(\\d+)-p")[,-1])][,MOD_RSD:=NULL]
+psp <- psp[res_kind=="Y"][,':='(res_kind=NULL,res=as.integer(res))][]
+psp[is.na(LT_LIT),LT_LIT:=0][is.na(MS_LIT),MS_LIT:=0][is.na(MS_CST),MS_CST:=0]
+psp[,myPSP:=LT_LIT>3|MS_LIT>2|MS_CST>27]
+setkey(psp,ACC_ID)
+#psp[psp[,rep(T,!any(myPSP)),by=.(ACC_ID)],myPSP:=NA][] it does not make secce to do this now
+psp[!(myPSP),myPSP:=NA]
+
+printSetDiffSizes(psp[,ACC_ID],uniprot_data[,canonic])
+
+
+setkey(sites,canonic,res)
+setkey(psp,ACC_ID,res)
+
+
+if(sites[psp,any(neighbourhood!=toupper(i.neighbourhood)),nomatch=0])
+  dfg
+if(sites[psp,any(GENE!=gene_name),nomatch=0])
+  dfg
+
+
+sites[,':='(LT_LIT=0L,MS_LIT=0L,MS_CST=0L)]
+sites[psp,':='(LT_LIT=i.LT_LIT,MS_LIT=i.MS_LIT,MS_CST=i.MS_CST),nomatch=0]
+
+
+
+
+# adding kinase specific data from phophositeplus ---------------
+
+pspSub = fread("../Data/Kinase_Substrate_Dataset.tsv",sep = "\t", 
+               select=c("KINASE","KIN_ACC_ID","GENE","KIN_ORGANISM","SUBSTRATE","SUB_ACC_ID","SUB_GENE" ,   
+                     "SUB_ORGANISM","SUB_MOD_RSD","SITE_+/-7_AA","IN_VIVO_RXN","IN_VITRO_RXN"))
+setnames(pspSub,"SITE_+/-7_AA","neighbourhood")
+pspSub = pspSub[KIN_ORGANISM=="human"][,':='(KIN_ORGANISM=NULL,
+                                             IN_VIVO_RXN=IN_VIVO_RXN=="X",
+                                             IN_VITRO_RXN=IN_VITRO_RXN=="X")][]
+pspSub[,c("res_kind","res"):=data.table(str_match(SUB_MOD_RSD,"^([YST])(\\d+)$")[,-1])][,SUB_MOD_RSD:=NULL]
+pspSub <- pspSub[res_kind=="Y"][,':='(res_kind=NULL,res=as.integer(res))][]
+
+
+the_nine=c('FYN (P06241)','ABL2 (P42684)','TNK1 (Q13470)','FRK (P42685)','FES (P07332)','PTK2 (Q05397)',
+           'SYK (P43405)','BMX (P51813)','JAK2 (O60674)')
+the_nine=setnames(data.table(str_match(the_nine,"(.+) \\((.+)\\)")),c("full_string","name","ACC"))
+
+setkey(pspSub,KIN_ACC_ID)
+setkey(the_nine,ACC)
+
+interesting_kinases=unique(c(pspSub[,.N,by=KIN_ACC_ID][N>10,KIN_ACC_ID],
+                      the_nine[pspSub[,.N,by=KIN_ACC_ID][N>1,KIN_ACC_ID],ACC,nomatch=0]))
+
+pspSub = pspSub[interesting_kinases,nomatch=0]
+
+pspSub_sites = pspSub[,setNames(lapply(interesting_kinases,function(x)x %in% KIN_ACC_ID),interesting_kinases),
+                                by=.(SUB_ACC_ID,res,neighbourhood)]
+setkey(pspSub_sites,SUB_ACC_ID)
+invisible(lapply(interesting_kinases,function(kinase)
+  pspSub_sites[pspSub_sites[,rep(T,!any(get(kinase))),by=.(SUB_ACC_ID)],(kinase):=NA][]
+))
+#melt(pspSub_sites[,4:34,with=F])[,.N,by=value]
+
+psp_sites = pspSub_sites[psp,on=c("SUB_ACC_ID"="ACC_ID","res"="res","neighbourhood"="neighbourhood")]
+fwritelist(psp_sites,"../Data/all_sites_pspPos.tsv")
+
+
+setkey(pspSub,KIN_ACC_ID,res)
+pspSub = pspSub[the_nine,nomatch=0]
+
+
+
+setkey(pspSub,SUB_ACC_ID,res)
+setkey(sites,canonic,res)
+
+sites_pspSub=pspSub[,.(kinases=list(GENE)),by=.(SUB_ACC_ID,res)]
+fwritelist(sites_pspSub,"../Data/sites_pspPos.tsv")
+
+sites[,kinases_psp:=rep(list(character()),.N)][sites_pspSub,kinases_psp:=kinases,nomatch=0]
+
+
+
+# adding mechismo output -------------
+
+filename="enFcexLbnj.site_table.tsv"
+folder=file.path("..","Data","Out")
+mechismo_out = fread(file.path(folder,filename),
+                     select=c("primary_id_a1", "primary_id_b1", "pos_a1", "mut_a1", 
+                              "user input", "mismatch", "conf", "ie","name_b1","idcode","iupred"))[primary_id_a1!=""]
+
+mechismo_out = mechismo_out[name_b1=="" | (name_b1=="[PROT]" ) | (primary_id_b1 %in% unique(uniprot_data$canonic))]
+setkey(mechismo_out,primary_id_a1,primary_id_b1,pos_a1,name_b1)
+mechismo_out = mechismo_out[mechismo_out[,.(pdb_ids=list(unique(idcode)),
+                                            mean_iupred=mean(iupred)),
+                                         by=.(primary_id_a1,primary_id_b1,pos_a1,name_b1)
+                                         ],
+                            mult="first"][,c("idcode","iupred"):=NULL]
+
+
+dropped = mechismo_out[mismatch!=0,.N,by=primary_id_a1]$primary_id_a1
+if (length(dropped) != 0) {
+  cat("Waring: dropped Entries with Interactor(s):\n ", paste0(dropped, collapse = ", "),
+      "\nbecause mechismo indicates that it used a different sequence as basis. (older uniprot sequence version)\n\n"
+  )
+  mechismo_out = mechismo_out[!(primary_id_a1 %in% dropped)]
+}
+mechismo_out[,mismatch:=NULL]
+
+mechismo_out = mechismo_out[mut_a1 == "Yp"][,mut_a1:=NULL]
+
+
+
+mechismo_sites = mechismo_out[,.(ie=list(ie[!is.na(ie)]),
+                iu_pred=mean(mean_iupred),
+                n_pdbs=length(unique(unlist(pdb_ids)))
+                ),by=.(primary_id_a1,pos_a1)]
+setkey(mechismo_sites,primary_id_a1, pos_a1)
+
+
+sites=mechismo_sites[sites,nomatch=0]
+
+
+fwritelist(sites,"../Data/sites.tsv")
+
+
+# unstructured regions are more likely to have phophorylated Tyrosins (ods ratio: 3.15 (2.27..4.31 95%cI) )
+fisher.test(table(sites[,.(iu_pred,phosphorylated_by_uniprot)]))
+sites[,mean(phosphorylated_by_uniprot),by=.(iu_pred)]
+
+
---
+++ 9_compare_Grossmann_Intact.R
@@ -0,0 +1,87 @@
+library(data.table)
+options(datatable.prettyprint.char = 25L)
+library(binom)
+
+
+
+source("misc.R")
+grossmann  = fread('../Data/IM-22632.txt',
+                   select=c(
+                     "Interaction identifier(s)",
+                     "Interaction annotation(s)",
+                     "Interaction detection method(s)",
+                     "#ID(s) interactor A",
+                     "ID(s) interactor B", 
+                     "Experimental role(s) interactor A",
+                     "Experimental role(s) interactor B",
+                     "Creation date"
+                     ), quote = ""
+                   )[`Interaction detection method(s)`=='psi-mi:"MI:0397"(two hybrid array)'
+                     ][,`Interaction detection method(s)`:= NULL
+                       ]
+setnames(grossmann,c("#ID(s) interactor A","ID(s) interactor B"),c("id.A","id.B"))
+ 
+# switch interactor A  nd B such that A is always prey:      
+grossmann[`Experimental role(s) interactor A`=="psi-mi:\"MI:0496\"(bait)",
+          ':='(id.A=id.B,
+               id.B=id.A
+          )
+          ][]
+
+# publication day of grossmann data
+day_zero = min(grossmann[, `Creation date`])
+
+grossmann = grossmann[str_detect(`Interaction annotation(s)`,"Phosphorylation-dependent interaction"),
+                       .(id.A,id.B)] # only keep pY dep PPIs and only the IDs
+
+grossmann %<>% extract(id.A,"id.A","(intact.*|[^-]*)(?:-[^-]*)*") %>% extract(id.B,"id.B","(intact.*|[^-]*)(?:-[^-]*)*") %>% unique()
+
+preys = unique(grossmann[,id.A])
+baits = unique(grossmann[,id.B])
+preysAndBaits = unique(c(preys,baits))
+
+
+intact = 
+  fread("../Data/intact/intact.txt", 
+        select = c(
+          "Interaction identifier(s)",
+          "Interaction annotation(s)",
+          "Interaction parameter(s)",
+          "Interaction detection method(s)",
+          "Publication Identifier(s)",
+          "#ID(s) interactor A",
+          "Creation date",
+          "Negative",
+          "ID(s) interactor B"
+          ), verbose = T, quote = ""
+        )[`Creation date`<day_zero & Negative=="false"]
+setnames(intact,c("#ID(s) interactor A","ID(s) interactor B"),c("id.A","id.B"))
+intact=intact[id.A %in% preysAndBaits][id.B %in% preysAndBaits]
+intact=rbind(intact,copy(intact)[,':='(id.A=id.B,id.B=id.A)])[id.A %in% preys][id.B %in% baits]
+setkey(intact, id.A,id.B)
+intact = unique(intact, by=c("id.A","id.B"))
+
+both_hit = intact[grossmann, nomatch=0]
+
+cat("Of the ", nrow(grossmann), " pY dependent PPIs found by Grossmann et al. ", 
+    nrow(both_hit), " (",nrow(both_hit)/nrow(grossmann)*100,"%) are found elsewhere as PPI on intact.",
+    "\nWhile of the ",length(preys)*length(baits), " PPP that Grossmann et al. could have found as pY PPI ", 
+    nrow(intact), " (",nrow(intact)/(length(preys)*length(baits))*100,"%)are found elsewhere as PPI on intact.",
+    sep="")
+#Of the 294 pY dependent PPIs found by Grossmann et al. 18 (6.122449%) are found elsewhere as PPI on intact.
+#While of the 11033 PPP that Grossmann et al. could have found as pY PPI 119 (1.078582%)are found elsewhere as PPI on intact.
+
+
+preys_in_intact = intact[, .N, keyby=id.A][preys][is.na(N),N:=0][]
+tmp <- binom.bayes(mean(preys_in_intact[,N]),length(preys))
+preys_in_intact[,p:=binom.bayes(N,length(preys), prior.shape1=tmp$shape1, prior.shape2=tmp$shape2)$mean]
+
+baits_in_intact = intact[, .N, keyby=id.B][baits][is.na(N),N:=0][]
+tmp <- binom.bayes(mean(baits_in_intact[,N]),length(baits))
+baits_in_intact[,p:=binom.bayes(N,length(baits), prior.shape1=tmp$shape1, prior.shape2=tmp$shape2)$mean]
+
+mean(CJ(preys_in_intact[,p], baits_in_intact[,p])[,V1*V2])
+
+baits_in_intact[preys_in_intact[grossmann,on="id.A",nomatch=0], on="id.B",nomatch=0][,.(E=sum(p*i.p),P=mean(p*i.p))]
+
+
